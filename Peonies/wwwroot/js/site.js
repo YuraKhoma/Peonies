db3f9d80612524ccdf821fc936bd9f6cabac6336
intern <intern@KPP-L-148>
2023-08-22 17:18:49 +0300

    Added angular controller

--- Peonies/wwwroot/js/site.js
+++ Peonies/wwwroot/js/site.js
@@ -8,18 +8,3 @@ phonecatApp.controller('MyController', function ($scope) {
     console.log(1221);
 });
 
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-    console.log(44444);
-    $scope.phones = [
-        {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
-        }
-    ];
-});
